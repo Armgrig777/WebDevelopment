38fbe30eb66a2979e361bf61a1b0e4e0b4c54c8c
Levon Fermanyan <fermanyanlevon@gmail.com>
2023-06-16 16:20:54 +0400

    Template Update

--- WebDevelopment/Controllers/HomeController.cs
+++ WebDevelopment/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using WebDevelopment.Data;
 using WebDevelopment.Models;
 
 namespace WebDevelopment.Controllers
@@ -7,13 +9,55 @@ namespace WebDevelopment.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Team()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Testimonials()
+        {
+            return View();
+        }
+
+        public IActionResult Faq()
+        {
+            return View();
+        }
+
+        public IActionResult Terms()
+        {
+            return View();
+        }
+
+        public IActionResult Cars()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
